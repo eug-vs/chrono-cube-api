593d8209241e9572a285d02d4b852fa0ed4c8aad
Eug-VS <eug-vs@keemail.me>
2020-01-01 22:22:23 +0300

    Add search filter to SolutionViewSet

--- timer/views.py
+++ timer/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 
 from timer.models import Solution
 from timer.serializers import SolutionSerializer
@@ -7,3 +8,5 @@ from timer.serializers import SolutionSerializer
 class SolutionViewSet(ModelViewSet):
     queryset = Solution.objects.all()
     serializer_class = SolutionSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=author__id', '=author__username']
